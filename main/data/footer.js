b1c68f2cfb3ab92d00267c60a2da56bada386323
Rafal <mailto:rafal.kobus@beliani.net>
2025-06-06 10:21:20 +0200

    Feet: Update: Telephone nr for FR are change at Company details

--- main/data/footer.js
+++ main/data/footer.js
@@ -456,7 +456,7 @@ export default [
     "Unsubscribe title": "Désabonnement",
     "Company Details": "Mentions légales",
     "Address": "Beliani (DE) GmbH, 6340 Baar, Suisse",
-    "Mobile number": "Téléphone: 01 87 39 86 15;",
+    "Mobile number": "Téléphone: +33 1 84 79 84 99;",
     "Email address": "E-mail:",
     "Mail to": "mailto:mail@beliani.fr",
     "Email": "mail@beliani.fr",
