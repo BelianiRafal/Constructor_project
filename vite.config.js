04eca8d1875f8a9f7c95a1978b6de3ea3f18da8a
kkazzann <contact@kznlabs.com>
2025-06-24 11:04:46 +0200

    feat: make vite use vite-plugin-vanilla to hmr js files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
-/** @type {import('vite').UserConfig} */
-export default {
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vanilla from 'vite-plugin-vanilla';
+
+export default defineConfig({
+  plugins: [
+    vanilla({
+      include: '**/*.html',
+      base: '/',
+      transform(html, ctx) {},
+    }),
+  ],
   server: {
     port: 5500,
   },
-};
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./', import.meta.url)),
+    },
+  },
+});
