f844a611ace2c42f90ab5f6219aefe679a93db03
kkazzann <kamil.kazaniecki@beliani.net>
2025-11-18 09:52:44 +0100

    feat: add keywords matcher to TopImageTitle (when line needs to be bigger)

--- components/components_remake/TopImageTitle.js
+++ components/components_remake/TopImageTitle.js
@@ -5,19 +5,31 @@ import { getState } from '../../utils/stateManager.js';
 
 export const TopImageTitle = isAllowToRender(
   optimize(({ href, src, title1, title2, color, type, renderType }) => {
+    const keywords = ["retrò", "retró", "rétro", "%", "halloween", "retro"]
+
+    function includesKeywords(title) {
+      for (let keyword of keywords) {
+        if (String(title).toLowerCase().includes(keyword)) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
 
     const templates = {
       up_to: `
-        <h4 style="color:${color};" class="${title1.includes('%') ? 'newsletterTitleH1' : 'newsletterTitleH2'}">${title1}</h4>
-        <h5 style="color:${color};" class="${title2?.includes('%') ? 'newsletterTitleH1' : 'newsletterTitleH2'}">${title2 ?? ''}</h5>
+        <h4 style="color:${color};" class="${includesKeywords(title1) ? 'newsletterTitleH1' : 'newsletterTitleH2'}">${title1}</h4>
+        <h5 style="color:${color};" class="${includesKeywords(title2) ? 'newsletterTitleH1' : 'newsletterTitleH2'}">${title2 ?? ''}</h5>
       `,
       up_to_bigger: `
-        <h4 style="color:${color};" class="${title1.includes('%') ? 'newsletterTitleH1Bigger' : 'newsletterTitleH1Bold'}">${title1}</h4>
-        <h5 style="color:${color};" class="${title2?.includes('%') ? 'newsletterTitleH1Bigger' : 'newsletterTitleH1Bold'}">${title2 ?? ''}</h5>
+        <h4 style="color:${color};" class="${includesKeywords(title1) ? 'newsletterTitleH1Bigger' : 'newsletterTitleH1Bold'}">${title1}</h4>
+        <h5 style="color:${color};" class="${includesKeywords(title2) ? 'newsletterTitleH1Bigger' : 'newsletterTitleH1Bold'}">${title2 ?? ''}</h5>
       `,
       line2bigger: `
-        <h4 style="color:${color};" class="${title1.includes('%') ? 'newsletterTitleH2BoldBigger' : 'newsletterTitleH1Bold'}">${title1}</h4>
-        <h4 style="color:${color};" class="${title2.includes('%') ? 'newsletterTitleH2BoldBigger' : 'newsletterTitleH1Bold'}">${title2}</h4>
+        <h4 style="color:${color};" class="${includesKeywords(title1) ? 'newsletterTitleH2BoldBigger' : 'newsletterTitleH1Bold'}">${title1}</h4>
+        <h4 style="color:${color};" class="${includesKeywords(title2) ? 'newsletterTitleH2BoldBigger' : 'newsletterTitleH1Bold'}">${title2}</h4>
       `,
       standard: `
         <h5 style="color:${color};" class="newsletterTitleH1">${title1}</h5>
@@ -30,6 +42,10 @@ export const TopImageTitle = isAllowToRender(
       singleLine: `
         <h4 style="color:${color};" class="newsletterTitleH1">${title1}</h4>
       `,
+      halloween: `
+        <h4 style="color:${color};" class="${String(title1).toLowerCase().includes('halloween') ? 'newsletterTitleH1' : 'newsletterTitleH2'}">${title1}</h4>
+        <h4 style="color:${color};" class="${String(title2).toLowerCase().includes('halloween') ? 'newsletterTitleH1' : 'newsletterTitleH2'}">${title2}</h4>
+      `
     };
 
 		if (renderType === "landing") {
@@ -50,7 +66,7 @@ export const TopImageTitle = isAllowToRender(
 			<tr>
 				<td style="line-height: 0; font-size: 0; padding: 0;">
 					<a href="${href}" style="display: block; text-decoration: none;">
-						<img alt="" src="${src}" style="display: block; width: 100%; max-width: 100%; height: auto; border: 0; line-height: 0;" loading="lazy">
+						<img alt="Top Image Title" src="${src}" style="display: block; width: 100%; max-width: 100%; height: auto; border: 0; line-height: 0;" loading="lazy">
 					</a>
 				</td>
 			</tr>`;
