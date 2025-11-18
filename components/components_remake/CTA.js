73abd2689ee626b99aae254ad09ade39a860ab6b
kkazzann <kamil.kazaniecki@beliani.net>
2025-11-18 09:53:29 +0100

    fix: add missing properties

--- components/components_remake/CTA.js
+++ components/components_remake/CTA.js
@@ -1,12 +1,12 @@
 import { Space } from './Space.js';
 
-export const CTA = ({ align = 'center', spaceBefore, spaceAfter, text, href }) => {
+export const CTA = ({ align = 'center', spaceBefore, spaceAfter, text, href, color = "#000000" }) => {
   const space_1 = spaceBefore ? Space({ className: spaceBefore?.class }) : '';
   const space_2 = spaceAfter ? Space({ className: spaceAfter?.class }) : '';
 
   const ctaInner = `
-		<td width="100%" style="text-align: center; color: #000000; text-decoration: underline;">
-			<a href="${href}" style="color: #000; text-decoration: underline;">
+		<td width="100%" style="text-align: center; color: ${color}; text-decoration: underline;">
+			<a href="${href}" style="color: ${color}; text-decoration: underline;">
 				<span class="newsletterCta">${text}</span>
 			</a>
 		</td>
