ffaef9f8dc7c71b16d287916cecc497a32a4e547
kkazzann <kamil.kazaniecki@beliani.net>
2025-11-18 09:54:09 +0100

    fix: update ProductsRow to use Product.js component

--- components/components_remake/ProductsRow.js
+++ components/components_remake/ProductsRow.js
@@ -1,7 +1,8 @@
 import { ImageWithLink } from './ImageWithLink_new.js';
 import { Space } from './Space.js';
+import { Product } from './Product.js';
 
-function generateProduct({ product, align, style, width = '100', showName, showPrices }) {
+function generateProduct({ product, align, style, width = '100', showName, showPrices = 'true' }) {
   return `
 	<td style="vertical-align: top; width: ${width}%">
         <table
@@ -13,83 +14,7 @@ function generateProduct({ product, align, style, width = '100', showName, showP
           <tbody>
             <tr>
               <td align="center">
-                <table cellspacing="0" cellpadding="0" align="center">
-                  <tbody>
-                    <!-- GÓRA -->
-                    <tr>
-                      <td align="center">
-                        <table
-                          border="0"
-                          cellspacing="0"
-                          cellpadding="0"
-                          width="100%"
-                        >
-                          <tbody>
-                            ${ImageWithLink({
-                              href: product.href,
-                              src: product.src,
-                              alt: product.name,
-                              insideRow: true,
-                            })}
-                          </tbody>
-                        </table>
-                      </td>
-                      <td></td>
-                    </tr>
-
-                    
-                    ${(showName === "false" && showPrices === "false") ? `
-                      <!-- DÓŁ -->
-                      <tr>
-                        <td align="center">
-                          <table
-                            cellspacing="0"
-                            cellpadding="0"
-                            style="width: 100%"
-                          >
-                            <tbody>
-                              ${Space({
-                                className: 'newsletterBottom20px',
-                              })}
-
-                              ${showName === "true" ? `
-                                <tr>
-                                  <td
-                                    align="${align}"
-                                    style="
-                                      padding-top: 0px;
-                                      padding-left: 0px;
-                                      padding-right: 0px;
-                                      padding-bottom: 0px;
-                                    "
-                                  >
-                                    <span class="newsletterProductTitle">
-                                      ${product.name}
-                                    </span>
-                                  </td>
-                                </tr>
-                                ` : ``}
-
-                              ${showPrices === "true" ? `
-                                <tr>
-                                  <td align="${align}">
-                                    <span class="newsletterProductLowPrice">
-                                      ${product.lowPrice}
-                                    </span>
-                                    <span class="newsletterProductHightPrice">
-                                      ${product.highPrice}
-                                    </span>
-                                  </td>
-                                </tr>
-                                ` : ``}
-                              
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                      ` : ``}
-                  </tbody>
-                </table>
+                ${Product(product, align, style, showPrices)}
               </td>
             </tr>
           </tbody>
@@ -98,7 +23,15 @@ function generateProduct({ product, align, style, width = '100', showName, showP
 			`;
 }
 
-export function ProductsRow(products, align = 'left', style, showName = "true", showPrices = "true") {
+export function ProductsRow({
+  products,
+  align = 'center',
+  style,
+  showName,
+  showPrices
+}) {
+
+  console.log('ProductsRow - showPrices:', showPrices);
   const productArray = Array.isArray(products)
     ? products
     : products.products
@@ -107,15 +40,28 @@ export function ProductsRow(products, align = 'left', style, showName = "true", 
 
   return `
 	<tr>
-	<td>
-		<table cellspacing="10px" width="100%" cellpadding="0" border="0" align="center" style="border-collapse: separate !important; border-spacing: 10px !important;">
+	<td class="newsletterContainer">
+		<table cellpadding="0" border="0" align="center" style="border-collapse: collapse;">
   <tbody>
     <tr>
       ${productArray
-        .map((product) =>
-          generateProduct({ product, align, style, width: 100 / productArray.length, showName, showPrices })
+        .map(
+          (product, index) =>
+            generateProduct({
+              product,
+              align,
+              style,
+              width: 100 / productArray.length,
+              // width: "33",
+              showName,
+              showPrices: showPrices,
+            }) +
+            (index !== productArray.length - 1
+              ? "<td class='newsletterProductSeparator'></td>"
+              : '')
         )
         .join('')}
+        
     </tr>
   </tbody>
 </table>
